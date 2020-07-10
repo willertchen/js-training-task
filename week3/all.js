03b9d5f7a6619b40f61a99053ede93d3dc8af8a6
EmL0777 <eml0777.chen@gmail.com>
2020-07-10 16:53:48 +0800

    feature/update

--- week3/all.js
+++ week3/all.js
@@ -3,6 +3,7 @@ new Vue({
   data: {
       products: [],
       uuid: 'd5cc7331-56ba-49f5-9c9f-6c2038f2b0a8',
+      token: '9eGzKr3bG6zhJlINdEk4DqjUW163r6VeZ1l4UMHIMrhQlw3ElSUeYzTdIWFj',
       tempProduct: {}
   },
   mounted () {
@@ -32,6 +33,43 @@ new Vue({
       .then(() => {
         $('#productModal').modal('show');  // 此處再接一個 .then() 以確保打個 Dialog 之前，能把資料下載好
       });
+    },
+    updateProduct() {
+
+      const url = new URL(
+          `https://course-ec-api.hexschool.io/api/${this.uuid}/admin/ec/product/${this.tempProduct.id}`
+      );
+
+      let headers = {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Bearer ${this.token}`,
+      };
+
+      let body = {
+          "title": this.tempProduct.title,
+          "category": this.tempProduct.category,
+          "content": this.tempProduct.content,
+          "description": this.tempProduct.description,
+          "imageUrl": [
+            this.tempProduct.imageUrl[0]
+          ],
+          "enabled": this.tempProduct.enabled,
+          "origin_price": this.tempProduct.origin_price,
+          "price": this.tempProduct.price,
+          "unit": this.tempProduct.unit,
+      };
+
+      fetch(url, {
+          method: "PATCH",
+          headers: headers,
+          body: body
+      })
+      .then(response => response.json())
+      .then(json => console.log(json));
+
+      // this.tempProduct = {};
+      // $('#productModal').modal('hide');
     }
   }
 });

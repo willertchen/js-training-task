ced5726c45ed747481b4b759a8c3e438c6d0c0d2
EmL0777 <eml0777.chen@gmail.com>
2020-07-10 14:40:22 +0800

    show data on dialog

--- week3/all.js
+++ week3/all.js
@@ -1,14 +1,37 @@
 new Vue({
   el: '#app',
-  data () {
-    return {
+  data: {
       products: [],
-      uuid: 'd5cc7331-56ba-49f5-9c9f-6c2038f2b0a8'
-    }
+      uuid: 'd5cc7331-56ba-49f5-9c9f-6c2038f2b0a8',
+      tempProduct: {}
   },
   mounted () {
     axios
       .get(`https://course-ec-api.hexschool.io/api/${this.uuid}/ec/products`)
       .then(response => (this.products = response.data.data));
+  },
+  methods: {
+    openModal(type, item) {
+      switch (type) {
+        case 'new':
+
+          break;
+        case 'edit':
+          this.getProduct(item.id);
+          break;
+        case 'delete':
+          break;
+        default:
+          break;
+      }
+    },
+    getProduct(id) {
+      axios
+      .get(`https://course-ec-api.hexschool.io/api/${this.uuid}/ec/product/${id}`)
+      .then(response => (this.tempProduct = response.data.data))
+      .then(() => {
+        $('#productModal').modal('show');  // 此處再接一個 .then() 以確保打個 Dialog 之前，能把資料下載好
+      });
+    }
   }
 });

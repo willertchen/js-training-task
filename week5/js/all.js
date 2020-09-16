6b31f76644f0de2113569783c9124ec44886e1ab
EmL0777 <eml0777.chen@gmail.com>
2020-09-16 23:03:51 +0800

    完成多國語系

--- week5/js/all.js
+++ week5/js/all.js
@@ -1,3 +1,5 @@
+// 匯入語系檔
+import zh_TW from './zh_TW.js';
 // 自定義設定檔，供外層綁定「錯誤的」className
 // 剛好跟 bootstrap 的樣式寫法是一樣的
 VeeValidate.configure({
@@ -6,6 +8,9 @@ VeeValidate.configure({
     invalid: 'is-invalid',
   }
 });
+
+// 將語系加入 VeeValidate 的設定檔案
+VeeValidate.localize('tw', zh_TW);
 
 // 載入 VeeValidate input 驗證工具，並註冊成全域元件
 Vue.component('ValidationProvider', VeeValidate.ValidationProvider);

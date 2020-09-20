bd72cf9124ac8bf2e5beeeb9dd05cb1797da485d
EmL0777 <eml0777.chen@gmail.com>
2020-09-20 20:41:37 +0800

    載入相關套件

--- week8/src/main.js
+++ week8/src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import jquery from 'jquery';
+import 'bootstrap';
+
+// Loading 套件
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 import App from './App.vue';
 import router from './router';
+
+window.$ = jquery;
+
+Vue.use(VueAxios, axios);
+Vue.component('Loading', Loading);
 
 Vue.config.productionTip = false;
 
